721f7f37cf986512bb9afe7d9427f0f3e9d934a4
Stéphane Depierrepont <toorop@toorop.fr>
2018-10-05 18:03:36 +0200

    routable link

---
+++ src/containers/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Loader from '../components/Loader'
+import MsgBox from '../components/MsgBox'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
+
+class Login extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Loader />
+                <Header />
+                <MsgBox />
+                <div>balbala</div>
+                <Footer />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Login
